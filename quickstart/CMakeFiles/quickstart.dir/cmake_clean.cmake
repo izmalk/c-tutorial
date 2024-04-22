5331e9fd4e6c5546e854989c45fd2a396e1c01c9
Vladimir Izmalkov <vladimir@vaticle.com>
2024-04-22 17:11:36 +0100

    Add a small C driver quickstart code smippet

---
+++ quickstart/CMakeFiles/quickstart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quickstart.dir/quickstart.c.o"
+  "CMakeFiles/quickstart.dir/quickstart.c.o.d"
+  "quickstart"
+  "quickstart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quickstart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
